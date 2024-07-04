9baab796e35407df4e90a043d20cd2992df6b4c8
Sophie Middleton <sophie@Sophies-MacBook-Pro.local>
2024-07-04 15:28:38 -0500

    residual plot updated

--- py-fitter/recoplot_module.py
+++ py-fitter/recoplot_module.py
@@ -3,6 +3,7 @@ import numpy as np
 import awkward as ak
 import math
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 from cycler import cycler
 
 # setup a cycler to get a different default color and linestyle
@@ -34,7 +35,7 @@ def PlotRecoMomEnt(branches, low, hi):
     plt.show()
 
 def plotmom_fit(data, fit_range, list_pdfs):
-    """ plot the final plot with fit and MC overlay, plus a residual plot """
+    """ plot the final plot with fit and data overlay, plus a residual plot """
     n_bins = 100
     mom_plot = np.linspace(fit_range[0], fit_range[1], n_bins)
     scale = 1 / n_bins * (fit_range[1] - fit_range[0])
@@ -43,7 +44,6 @@ def plotmom_fit(data, fit_range, list_pdfs):
     data_bincenter = 0.5 * (data_binedge[1:] + data_binedge[:-1])
 
     fig, (ax1, ax2) = plt.subplots(2,1, height_ratios=[3,1])
-    #ax1.hist(data, color='black', bins=n_bins, range=fit_range, histtype='stepfilled', alpha=0.1)
     ax1.hist(data, color='black', bins=n_bins, range=fit_range, histtype='step')
     ax1.errorbar(data_bincenter, data_hist, yerr=np.sqrt(data_hist), color='None', ecolor='black', capsize=3)
 
@@ -54,16 +54,8 @@ def plotmom_fit(data, fit_range, list_pdfs):
         pdf_plot = (pdfs.pdf(mom_plot) * N_pdfs * scale).numpy()
         combine_plot += pdf_plot
         ax1.plot(mom_plot, pdf_plot, label=name)
-    #ce_plot = (ce.pdf(mom_plot) * N_CE * scale).numpy()
-    #dio_plot = (dio.pdf(mom_plot) * N_DIO * scale).numpy()
-    #cosmic_plot = (cosmic.pdf(mom_plot) * N_Cosmic * scale).numpy()
-    #combine_plot = ce_plot + dio_plot + cosmic_plot
 
-    #ax1.plot(mom_plot, ce_plot, '--', color='blue', label='CE')
-    #ax1.plot(mom_plot, dio_plot, ':', color='green', label='DIO')
-    #ax1.plot(mom_plot, cosmic_plot, '-.', color='orange', label='Cosmic')
     ax1.plot(mom_plot, combine_plot, '-r', label='Total')
-
     ax1.grid(True)
     ax1.set_yscale('log')
     ax1.set_xlim(fit_range) #FIXME change range to variables
@@ -75,6 +67,8 @@ def plotmom_fit(data, fit_range, list_pdfs):
     ax2.errorbar(mom_plot, np.abs(combine_plot - data_hist)/data_hist, yerr=err, color='None', marker='+', markerfacecolor='black', ecolor='black', capsize=3)
 
     ax2.grid(True)
+    ax2.yaxis.set_ticks(np.arange(-5, 5,2))
+    ax2.yaxis.set_minor_formatter(ticker.FormatStrFormatter('%0.1f'))
     ax2.set_xlim(fit_range)
     ax2.set_xlabel('Reconstructed Momentum [MeV/c]')
     ax2.set_ylabel('Normalized Residual')
