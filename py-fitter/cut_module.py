45e7f663efd196559af51b9958b7a4734a0a2d0b
lborrel <lborrel@caltech.edu>
2024-11-04 18:09:41 -0600

    Add compatibility with EventNtuple v6

--- py-fitter/cut_module.py
+++ py-fitter/cut_module.py
@@ -13,47 +13,43 @@ class CutClass:
         self.Cut_List = {}
         if opt == 'MDC2024': #TODO these are SU2020 cuts, and some are missing....
             self.Cut_List = {
-                "demfit.time" : [640., 1650], #inTimeWindow
-                "demlh.t0err.max" : [0.9], #intimeErr
-                "demlh.maxr" : [450., 680.], #inMaxRCut
-                "demtrkqual.result" : [0.2], # trk qual
-                "dem.nactive" : [20], # active hits in fit
-                "time_diff" : [50.,150.] # cut anything with a track - crv time difference less than this
+                "trk.nactive" : [20, float('inf')], # active hits in the tracker
+                #"trk.sid" : [0], # select values at the entrance of the tracker
+                #"trksegs.time" : [640., 1650], #inTimeWindow
+                #"trksegpars_lh.t0err" : [0, 0.9], #intimeErr
+                #"trksegpars_lh.maxr" : [450., 680.], #inMaxRCut
+                #"trkqual.result" : [0.2, float('inf')], # trk qual
+                "crv_coincidence" : [50.,150.] # cut anything with a track - crv time difference less than this
             }
 
-    def ApplyCut_mom(self, demfits, trkqual, demhits, crvcoin = None):
+    def ApplyCut(self, array_trk, array_crv):
         """ function applies cuts to MDC2024 trkana """
+        print("Applying cuts\n")
+        print("array size before cut: ", ak.num(array_trk, axis=0))
 
-        # find magnitude
-        demfits['demfit_mom'] = np.sqrt((demfits['demfit']['mom']['fCoordinates']['fX'])**2 + (demfits['demfit']['mom']['fCoordinates']['fY'])**2 + (demfits['demfit']['mom']['fCoordinates']['fZ'])**2)
+        for key, value in self.Cut_List.items():
+            print(key)
 
-        # select trak fit at entrance
-        trk_ent_mask = (demfits['demfit']['sid']==0)
+            if key == 'crv_coincidence':
+                print("crv coincidence")
 
-        # build masks for cuts
-        time_cut_mask_min = (demfits['demfit']['time'] >= self.Cut_List["demfit.time"][0])
-        time_cut_mask_max = (demfits['demfit']['time'] < self.Cut_List["demfit.time"][1])
-        timeerr_cut_mask = (demfits['demlh']['t0err'] < self.Cut_List["demlh.t0err.max"][0])
-        maxr_cut_mask = (demfits['demlh']['maxr'] > self.Cut_List["demlh.maxr"][0])
-        active_mask = (demhits['dem.nactive'] > self.Cut_List["dem.nactive"][0])
+            else:
+                if type(value) == int:
+                    mask = array_trk[key] == value
+                elif len(value) == 2:
+                    mask = (array_trk[key] >= value[0]) & (array_trk[key] <= value[1])
 
-        # apply trkqual cut # FIXME - commented here due to TrkAna v5 issue
-        # trkqual_mask = (trkqual['demtrkqual.result'] > self.Cut_List["demtrkqual.result"][0])
+            print("# of events passing this cut: ", ak.sum(mask))
 
-        # look for CRV coincidences
-        crv_cut_mask = self.ApplyCRVCut(demfits, crvcoin, self.Cut_List['time_diff'][1])
-
-        # apply all cuts and convert to numpy array
-        data_cut = demfits[(active_mask) & (crv_cut_mask) & (trk_ent_mask) & (time_cut_mask_max) & (time_cut_mask_min) & (timeerr_cut_mask) & (maxr_cut_mask)]
-        return data_cut
+        return array_trk
 
     def ApplyCRVCut(self, demfits, crvcoin, cut_value):
         """ function applies time based cut on comparison of TRK and CRV times """
         # Remove events without any track
-        trk_valid = ak.num(demfits['demfit','time'], axis=1) > 0
+        trk_valid = ak.num(demfits['trksegs','time'], axis=1) > 0
         mask_trk = ak.mask(demfits, trk_valid)
         crv_coinc = np.ones(ak.num(demfits, axis=0), dtype='bool')    # Create a boolean array of True for the mask
-        for evt_idx, evt in enumerate(demfits['demfit','time']):   # loop through every event
+        for evt_idx, evt in enumerate(demfits['trksegs','time']):   # loop through every event
             for trk_idx, trk in enumerate(evt):     # loop through every track within an event
                 if ak.num(trk, axis=0) > 0:       # rare case when an event has 2 tracks, with one being empty (Ask why?)
                     for crv_idx, crv in enumerate(crvcoin['crvcoincs.time', evt_idx]):    # loop through CRV hits
--- py-fitter/import_module.py
+++ py-fitter/import_module.py
@@ -15,32 +15,30 @@ class ImportClass :
         self.BranchName = branchName
         self.Array = ak.Array
 
-    def Import(self):
+
+    def Import(self, filter_branch="*"):
         """ Import root tree and save it as an Awkward array """
         input_file = uproot.open(self.FileName)
         input_tree = input_file[self.TreeName][self.BranchName]
+        self.Array = input_tree.arrays(filter_name = filter_branch, library='ak')
 
-        # Uproot5 using awkward array
-        self.Array = input_tree.arrays(library='ak')
         return self.Array
 
-    def Import_branches(self, leafnames):
-        """ import list of branches from trk ana"""
-        trkana = uproot.open(self.FileName+":"+str(self.TreeName)+"/"+str(self.BranchName))
-        list_names = []
-        for i, leafname  in enumerate(leafnames):
-            #branches = trkana.arrays(filter_name=["/"+str(leafname)+"/", "/"+str(leafname_field)+"/"])
-            list_names.append("/"+str(leafname)+"/")
-        branches = trkana.arrays(filter_name=list_names)
-        ak.flatten(branches, axis=None)
-        return branches
+    def Import_branch(self, branch_name):
+        """ Import only one single branch in an awkward array """
+        input_file = uproot.open(self.FileName)
+        input_tree = input_file[self.TreeName][self.BranchName]
+        self.Array = input_tree[branch_name].array(library='ak')
 
-    def Import_mom(self, array_MC):
-        """ import array select reco mom """
-        array_MC['demfit_mom'] = np.sqrt((array_MC['demfit']['mom']['fCoordinates']['fX'])**2 + (array_MC['demfit']['mom']['fCoordinates']['fY'])**2 + (array_MC['demfit']['mom']['fCoordinates']['fZ'])**2)
-        trk_ent_mask = (array_MC['demfit']['sid']==0)
-        data_np = np.array(ak.flatten(array_MC[(trk_ent_mask)]['demfit_mom']))
-        return data_np
+        return self.Array
+
+
+    def AddMomentumBranch(self, array_trk):
+        """ Add momentum branch """
+        array_trk['trksegs.mom.mag'] = np.sqrt((array_trk['trksegs','mom','fCoordinates','fX'])**2 + (array_trk['trksegs','mom','fCoordinates','fY'])**2 + (array_trk['trksegs','mom','fCoordinates','fZ'])**2)
+
+        return array_trk
+
 
     def printAllField(self):
         """Print all the field variable in the array with their type"""
